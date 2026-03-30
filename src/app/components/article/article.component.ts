1b17248bcccdba607c922f95246d407d4d38d751
nteper <nteper@codemastersintl.com>
2026-03-30 14:34:23 +0300

    Readme file

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
-import { Form, ReactiveFormsModule, FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+} from '@angular/forms';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { IArticle } from '../../models/article';
 import { ArticleService } from '../../services/article.service';
@@ -10,26 +16,33 @@ import { BehaviorSubject } from 'rxjs';
   selector: 'app-article',
   imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './article.component.html',
-  styleUrl: './article.component.scss'
+  styleUrl: './article.component.scss',
 })
 export class ArticleComponent {
   protected readonly articleForm: FormGroup;
-  protected readonly article$: BehaviorSubject<IArticle|null> = new BehaviorSubject<IArticle|null>(null);
+  protected readonly article$: BehaviorSubject<IArticle | null> =
+    new BehaviorSubject<IArticle | null>(null);
   protected edit: boolean = false;
-  constructor(@Inject(DOCUMENT) private document: Document, private localStorage: LocalStorageService, private articleService: ArticleService) {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private localStorage: LocalStorageService,
+    private articleService: ArticleService,
+  ) {
     this.articleForm = new FormGroup({
-      "articleTitle": new FormControl('New article', Validators.required),
-      "articleContent": new FormControl('', Validators.maxLength(500)),
+      articleTitle: new FormControl('New article', Validators.required),
+      articleContent: new FormControl('', Validators.maxLength(500)),
     });
     this.article$ = this.articleService.article$;
   }
 
   protected submit() {
     const article: IArticle = {
-      id: this.articleForm.controls.id ? this.articleForm.controls.id.value : crypto.randomUUID(),
+      id: this.articleForm.controls.id
+        ? this.articleForm.controls.id.value
+        : crypto.randomUUID(),
       title: this.articleForm.controls.articleTitle.value,
-      content: this.articleForm.controls.articleContent.value
-    }
+      content: this.articleForm.controls.articleContent.value,
+    };
     this.localStorage.setItem(article.id, article);
     this.localStorage.setIds(article.id);
     this.localStorage.setList();
@@ -42,8 +55,8 @@ export class ArticleComponent {
     this.articleForm.patchValue({
       id: article.id,
       articleTitle: article.title,
-      articleContent: article.content
-    })
+      articleContent: article.content,
+    });
   }
 
   protected cancel() {
@@ -55,7 +68,5 @@ export class ArticleComponent {
     if (selection && selection.rangeCount > 0) {
       console.log('test', selection);
     }
-    
   }
 }
-
